dbd94a272cace1b8c2019c258b4d1567bee2e031
Inês Carvalho <4671109+imcarvalho@users.noreply.github.com>
2023-02-05 19:02:41 +0000

    add tap dance

--- keyboards/preonic/keymaps/imcarvalho/keymap.c
+++ keyboards/preonic/keymaps/imcarvalho/keymap.c
@@ -28,21 +28,34 @@ enum preonic_keycodes {
   BACKLIT
 };
 
+
+//Tap Dance Declarations
+enum {
+  TD_SPC_ENT = 0
+};
+
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for space, twice for enter
+  [TD_SPC_ENT]  = ACTION_TAP_DANCE_DOUBLE(KC_SPC, KC_ENT)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_preonic_grid(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_EQL,  KC_EQL,
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
   KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-  KC_LCTL, KC_LALT, KC_LGUI, KC_LEFT, KC_RGHT, KC_SPC,  KC_SPC,  KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(_COLEMAK)
+  KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+  KC_LCTL, KC_LALT, KC_LGUI, KC_LEFT, KC_RGHT, TD(TD_SPC_ENT),TD(TD_SPC_ENT),KC_UP,KC_DOWN, KC_LBRC, KC_RBRC, MO(_COLEMAK)
 ),
 
 [_COLEMAK] = LAYOUT_preonic_grid(
   QK_BOOT, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_MOD, RGB_SPI, RGB_SPD,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 };
--- keyboards/preonic/keymaps/imcarvalho/rules.mk
+++ keyboards/preonic/keymaps/imcarvalho/rules.mk
@@ -2,3 +2,4 @@ SRC += muse.c
 AUTO_SHIFT_ENABLE = yes
 VIA_ENABLE = yes
 LTO_ENABLE = yes
+TAP_DANCE_ENABLE=yes
